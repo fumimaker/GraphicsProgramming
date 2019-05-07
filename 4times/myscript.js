53651a4562e600a91f2912551ee7050a009c957c
fumimaker <fumi01200924@gmail.com>
2019-05-07 16:26:09 +0900

    WIP

--- 4times/myscript.js
+++ 4times/myscript.js
@@ -1,16 +1,21 @@
+var time = 0;
 function setup(){
-    createCanvas(400,400);
-    background(200);
+    createCanvas(1000,1000);
+    background(255);
 }
 
 function draw(){
+    
     for(var j=0; j<10; j++){
         for(var i=0; i<10; i++){
+            
             noStroke();
-            fill(i*25,i*25-j*25,j*25);
-            rect(0,0,39,39);
-            translate(0, 40, 0);
+            fill(i * 25, i * 25 - j * 25, j * 25);
+            ellipse(i + 10, j + 10, 20, 20);
+
+            translate(j, 40, j);
         }
         translate(40,-400,0);
     }
+
 }
