8ed260d83ae5f037f417207509ef2cab7bd50a42
fumimaker <fumi01200924@gmail.com>
2019-06-26 15:27:45 +0900

    update readme

--- 11times/sketch.js
+++ 11times/sketch.js
@@ -16,8 +16,7 @@ function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.size(width, height);
-  img = createImg('https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png');
-  //img.hide();
+
   // Create a new poseNet method with a single detection
   poseNet = ml5.poseNet(video, modelReady);
   // This sets up an event that fills the global variable "poses"
@@ -33,24 +32,66 @@ function modelReady() {
   select('#status').html('Model Loaded');
 }
 
-function mousePressed(){
-  console.log(JSON.stringify(poses))
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
 }
 
-function draw() {
-  img.hide();
-  image(video, 0, 0, width, height);
-  //strokeWeight(2);
-  noStroke();
-  // For one pose only (use a for loop for multiple poses!)
-  if (poses.length > 0) {
-    let pose = poses[0].pose;
-
-    // Create a pink ellipse for the nose
-    fill(255, 0, 0);
-    let nose = pose['nose'];
-    //ellipse(nose.x, nose.y, 10, 10);
-    image(img,nose.x-180,nose.y-180);
-
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.9) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
   }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
+
+function drawStar(x, y, r, prickleNum) {
+  var vertexNum = prickleNum * 2;  // 頂点数(トゲの数*2)
+  var R;  // 中心点から頂点までの距離
+  var outR = r;  // 中心点からトゲまでの距離
+  var inR = outR / 2;  // 中心点から谷までの距離
+
+  //pushMatrix();
+  translate(x, y);
+  rotate(radians(-90));
+  beginShape();
+  for (var i = 0; i < vertexNum; i++) {
+    if (i % 2 == 0) {
+      R = outR;
+    } else {
+      R = inR;
+    }
+    vertex(R * cos(radians(360 * i / vertexNum)), R * sin(radians(360 * i / vertexNum)));
+  }
+  endShape(CLOSE);
+  //popMatrix();
 }
