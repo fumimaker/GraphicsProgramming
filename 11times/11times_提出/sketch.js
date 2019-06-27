ae36f1d9d31659b84af9abd87b07959f537cd323
fumimaker <fumi01200924@gmail.com>
2019-06-27 18:35:57 +0900

    そのまま提出してた

---
+++ 11times/11times_提出/sketch.js
@@ -0,0 +1,56 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
+let video;
+let poseNet;
+let poses = [];
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  img = createImg('https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png');
+  //img.hide();
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function (results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+  select('#status').html('Model Loaded');
+}
+
+function mousePressed() {
+  console.log(JSON.stringify(poses))
+}
+
+function draw() {
+  img.hide();
+  image(video, 0, 0, width, height);
+  //strokeWeight(2);
+  noStroke();
+  // For one pose only (use a for loop for multiple poses!)
+  if (poses.length > 0) {
+    let pose = poses[0].pose;
+
+    // Create a pink ellipse for the nose
+    fill(255, 0, 0);
+    let nose = pose['nose'];
+    //ellipse(nose.x, nose.y, 10, 10);
+    image(img, nose.x - 180, nose.y - 180);
+
+  }
+}
