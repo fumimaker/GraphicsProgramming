54b3e9f7974102f772dd9ed140ef9db904781483
fumimaker <fumi01200924@gmail.com>
2019-07-02 15:06:20 +0900

    create 12times and add collider2D

---
+++ 12times/sketch.js
@@ -0,0 +1,15 @@
+//var posX;
+//var posY;
+var pos;
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  //posX = random(width);
+  //posY = random(height);
+
+  pos = createVector(random(width),randon(height));
+}
+
+function draw(){
+
+}
