0724a5400e61c68c28efe1e6f19a24290eb50015
fumimaker <fumi01200924@gmail.com>
2019-07-02 15:44:27 +0900

    Collideの接触判定で色を帰ることができる

--- 12times/sketch.js
+++ 12times/sketch.js
@@ -9,16 +9,26 @@ function setup(){
   pos = createVector(random(width),random(height));
   speed = createVector(random(-10,10), random(-10,10));
   rad = 100;
+  objectPos = createVector(width/2, height/2);
 }
 
 function draw(){
   pos.add(speed);
 
   var hit;
-  hit = colliderCircleCircle(pos.x, pos.y, rad, objectPos.x, objectPos.y, rad);
+  hit = collideCircleCircle(pos.x, pos.y, rad, objectPos.x, objectPos.y, rad);
 
   background(255,190,240);
+
+  if(hit){
+    fill(100,100,100);
+  }
+  else{
+    fill(100,200,100);
+  }
+
   circle(pos.x, pos.y, rad);
+  circle(objectPos.x, objectPos.y, rad);
 
   if(pos.x < 0 + rad/2){
     pos.x = rad/2;
