4b096617c3ee17b0c6c8e3f915096736ed4d1f81
fumimaker <fumi01200924@gmail.com>
2019-07-02 15:38:19 +0900

    colliderで反射に成功

--- 12times/sketch.js
+++ 12times/sketch.js
@@ -6,10 +6,34 @@ function setup(){
   createCanvas(windowWidth, windowHeight);
   //posX = random(width);
   //posY = random(height);
-
-  pos = createVector(random(width),randon(height));
+  pos = createVector(random(width),random(height));
+  speed = createVector(random(-10,10), random(-10,10));
+  rad = 100;
 }
 
 function draw(){
+  pos.add(speed);
 
+  var hit;
+  hit = colliderCircleCircle(pos.x, pos.y, rad, objectPos.x, objectPos.y, rad);
+
+  background(255,190,240);
+  circle(pos.x, pos.y, rad);
+
+  if(pos.x < 0 + rad/2){
+    pos.x = rad/2;
+    speed.x *= -1.0;
+  }
+  else if(pos.x > width - rad/2){
+    pos.x = width - rad/2;
+    speed.x *= -1.0;
+  }
+  else if(pos.y<0 + rad/2){
+    pos.y = rad/2;
+    speed.y *= -1.0;
+  }
+  else if(pos.y > height - rad/2){
+    pos.y = height - rad/2;
+    speed.y *= -1.0;
+  }
 }
