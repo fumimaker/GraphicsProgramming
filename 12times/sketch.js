dcaeb8f941165558e9cb8679874acbdcafb7ff11
fumimaker <fumi01200924@gmail.com>
2019-07-03 14:01:54 +0900

    Update sketch.js

--- 12times/sketch.js
+++ 12times/sketch.js
@@ -1,20 +1,33 @@
+/*
 var pos=[];
 var speed = [];
 var rad = [];
+*/
+var pos, speed,rad;
+
 var numParticle;
 var objectPos;
 
 function setup(){
   numParticle = 20;
   createCanvas(windowWidth, windowHeight);
+  
+  pos = createVector(random(width), random(height));
+  speed = createVector(random(-10, 10), random(-10, 10));
+  rad = random(200);
+  
+  /*
   for(var i=0; i<numParticle; i++){
     pos[i] = createVector(random(width), random(height));
     speed[i] = createVector(random(-10, 10), random(-10, 10));
     rad[i] = random(200);
   }
+  */
 }
 
 function draw(){
+  background(255, 190, 240);
+  /*
   for(var i=0; i<numParticle; i++){
     pos[i].add(speed[i]);
     for(var j=0; j<numParticle; j++){
@@ -30,25 +43,26 @@ function draw(){
       }
     }
   }
-  background(255, 190, 240);
-  for(var i=0; i<numParticlel; i++){
-    if (pos[i].x < 0 + rad[i] / 2) {
-      pos[i].x = rad[i] / 2;
-      speed[i].x *= -1.0;
+  */
+ pos.add(speed);
+  for(var k=0; k<numParticlel; k++){
+    if (pos[k].x < 0 + rad[k] / 2) {
+      pos[k].x = rad[k] / 2;
+      speed[k].x *= -1.0;
     }
-    if (pos[i].x > width - rad[i] / 2) {
-      pos[i].x = width - rad[i] / 2;
-      speed[i].x *= -1.0;
+    if (pos[k].x > width - rad[k] / 2) {
+      pos[k].x = width - rad[k] / 2;
+      speed[k].x *= -1.0;
     }
-    if (pos[i].y < 0 + rad[i] / 2) {
-      pos[i].y = rad[i] / 2;
-      speed[i].y *= -1.0;
+    if (pos[k].y < 0 + rad[k] / 2) {
+      pos[k].y = rad[k] / 2;
+      speed[k].y *= -1.0;
     }
-    if (pos[i].y > height - rad[i] / 2) {
-      pos[i].y = height - rad[i] / 2;
-      speed[i].y *= -1.0;
+    if (pos[k].y > height - rad[k] / 2) {
+      pos[k].y = height - rad[k] / 2;
+      speed[k].y *= -1.0;
     }
     fill(255,255,255);
-    circle(pos[i].x, pos[i].y, rad[i]);
+    circle(pos[k].x, pos[k].y, rad[k]);
   }
 }
